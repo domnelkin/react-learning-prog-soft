972147537357843c9bae7f6af066e912be1a42ab
Nelia Zbyshko <nelyazbishko@gmail.com>
2021-09-22 23:03:49 +0300

    css

--- src/components/MainBlock.js
+++ src/components/MainBlock.js
@@ -6,11 +6,10 @@ const MainBlock = (props) => {
         <div className='main-content'>
             <div class="main-img">
                 <img src={props.img} alt='Image Placeholder' />
-                <video src='/videos/Video-1.mp4' autoPlay loop muted />
             </div>
             <div class="main-block-text-content">
                 <a href={props.link}><h3>{props.title}</h3></a>
-                <div><p class="main-block-small-text">{props.text}</p></div>
+                <div class="content-of-small-text"><p class="main-block-small-text">{props.text}</p></div>
             </div>
         </div>
     );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -10,8 +10,15 @@ const Home = () => {
             <div className='content'>
                 <MainBlock img='images\MainPhoto-1.jpg'
                     title='A picture is a poem without words.'
-                    text='sufjdsugbdsudjaIDN ASFSDJGBSD SHGBSHDBVDSJ GDSGHFUHASJIR SDGJB'
-                    link='/'
+                    text='Art is a highly diverse range of human activities engaged in creating visual, auditory, or performed artifacts— artworks—that express the author’s imaginative or technical skill, and are intended to be appreciated for their beauty or emotional power.
+
+                    The oldest documented forms of art are visual arts, which include images or objects in fields like painting, sculpture, printmaking , photography, and other visual media . Architecture is often included as one of the visual arts; however, like the decorative arts, it involves the creation of objects where the practical considerations of use are essential, in a way that they usually are not in another visual art, like a painting.
+                    
+                    Art may be characterized in terms of mimesis (its representation of reality), expression, communication of emotion, or other qualities. Though the definition of what constitutes art is disputed and has changed over time, general descriptions center on the idea of imaginative or technical skill stemming from human agency and creation. When it comes to visually identifying a work of art, there is no single set of values or aesthetic traits. A Baroque painting will not necessarily share much with a contemporary performance piece, but they are both considered art.
+                    
+                    Despite the seemingly indefinable nature of art, there have always existed certain formal guidelines for its aesthetic judgment and analysis. Formalism is a concept in art theory in which an artwork’s artistic value is determined solely by its form, or how it is made. Formalism evaluates works on a purely visual level, considering medium and compositional elements as opposed to any reference to realism , context, or content.
+                    
+                    Art is often examined through the interaction of the principles and elements of art. The principles of art include movement, unity, harmony, variety, balance, contrast, proportion and pattern. The elements include texture, form, space, shape, color, value and line. The various interactions between the elements and principles of art help artists to organize sensorially pleasing works of art while also giving viewers a framework within which to analyze and discuss aesthetic ideas.'                    link='/'
                 />
             </div>
             <div className='blog'>
