2de72f7b705990394c61364e0d1e25f3dea2698e
Nelia Zbyshko <nelyazbishko@gmail.com>
2021-09-22 17:12:30 +0300

    init css added

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './HeaderButton.css';
 import { Link } from 'react-router-dom';
 
 const HeaderButton = (props) => {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './Navbar.css';
 import HeaderButton from "./HeaderButton";
 import { Link } from 'react-router-dom';
 
