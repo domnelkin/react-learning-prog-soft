0bf30f1f514f07785ae9b5f1824f537edf641e96
Nelia Zbyshko <nelyazbishko@gmail.com>
2021-09-23 00:02:57 +0300

    added heading for home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -21,23 +21,24 @@ const Home = () => {
                     Art is often examined through the interaction of the principles and elements of art. The principles of art include movement, unity, harmony, variety, balance, contrast, proportion and pattern. The elements include texture, form, space, shape, color, value and line. The various interactions between the elements and principles of art help artists to organize sensorially pleasing works of art while also giving viewers a framework within which to analyze and discuss aesthetic ideas.'                    link='/'
                 />
             </div>
+            <div className='main-text-content'><h1>The best shop of ARTS</h1></div>
             <div className='blog'>
                 <NewsBlock
                     img='images\Photo-1.jpg'
-                    title='Test News Block'
-                    text='sufjdsugbdsudjaIDN ASFSDJGBSD SHGBSHDBVDSJ GDSGHFUHASJIR SDGJB'
+                    title='What Does Art Mean?'
+                    text='The meaning of art is shaped by the intentions of the artist as well as the feelings and ideas it engenders in the viewer.'
                     link='/'
                 />
                 <NewsBlock
                     img='images\Photo-2.jpg'
-                    title='Test News Block'
-                    text='sufjdsugbdsudjaIDN ASFSDJGBSD SHGBSHDBVDSJ GDSGHFUHASJIR SDGJB'
+                    title='What Makes Art Beautiful?'
+                    text='Beauty in terms of art refers to an interaction between line, color, texture, sound, shape, motion, and size that is pleasing to the senses.'
                     link='/'
                 />
                 <NewsBlock
                     img='images\Photo-3.jpg'
-                    title='Test News Block'
-                    text='sufjdsugbdsudjaIDN ASFSDJGBSD SHGBSHDBVDSJ GDSGHFUHASJIR SDGJB'
+                    title='Who Is an Artist?'
+                    text='An artist is a person who is involved in the wide range of activities that are related to creating art.'
                     link='/'
                 />
             </div>
