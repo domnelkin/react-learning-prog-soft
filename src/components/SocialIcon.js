01b1995bfbaec883943e7686582dc74c4c353b2f
Nelia Zbyshko <nelyazbishko@gmail.com>
2021-09-24 00:54:55 +0300

    footer added

--- src/components/SocialIcon.js
+++ src/components/SocialIcon.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 const SocialIcon = (props) => {
     return (
-        <Link to={props.link} className='nav-link'>
-            <img src={props.img} alt="social" />
-        </Link>
+        <a href={props.link} className='social-btn'>
+            <img src={props.img} alt="social" class="social-img" />
+        </a>
     );
 }
 
